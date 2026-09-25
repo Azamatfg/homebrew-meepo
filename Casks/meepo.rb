fafc551e1e2424f4f05c9218106a724577215c14
Azamat Bigali <azamatfg@gmail.com>
2026-09-25 19:52:53 +0500

    meepo 0.2.7-beta

--- Casks/meepo.rb
+++ Casks/meepo.rb
@@ -1,6 +1,6 @@
 cask "meepo" do
-  version "0.2.6-beta"
-  sha256 "5cfc497a2590fcc560ee2c15c3d3173d9cc672c6970eca5371d7c9c3729de685"
+  version "0.2.7-beta"
+  sha256 "4bc117df30c6beffcb76c6347aeda009748b073e6a77aa8e6968839ec3f9d9dd"
 
   url "https://github.com/Azamatfg/meepo/releases/download/v#{version}/Meepo.zip"
   name "Meepo"
